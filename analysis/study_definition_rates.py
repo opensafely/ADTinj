4980299ddb3f971eaca34f1193decbbffbc5343c
lemanska <a.lemanska@surrey.ac.uk>
2022-05-19 22:17:19 +0100

    remove ethn from measure

--- analysis/study_definition_rates.py
+++ analysis/study_definition_rates.py
@@ -36,39 +36,6 @@ study = StudyDefinition(
             "category": {"ratios": {"M": 0.49, "F": 0.51}},
         }
     ),
-    ethnicity=patients.categorised_as(
-        {
-            "Missing": "DEFAULT",
-            "White": """ ethnicity_code=1 """,
-            "Mixed": """ ethnicity_code=2 """,
-            "South Asian": """ ethnicity_code=3 """,
-            "Black": """ ethnicity_code=4 """,
-            "Other": """ ethnicity_code=5 """,
-        },
-        return_expectations={
-            "rate": "universal",
-            "category": {
-                "ratios": {
-                    "Missing": 0.4,
-                    "White": 0.2,
-                    "Mixed": 0.1,
-                    "South Asian": 0.1,
-                    "Black": 0.1,
-                    "Other": 0.1,
-                }
-            },
-        },
-        ethnicity_code=patients.with_these_clinical_events(
-            ethnicity_codes,
-            returning="category",
-            find_last_match_in_period=True,
-            on_or_before="index_date",
-            return_expectations={
-            "category": {"ratios": {"1": 0.4, "2": 0.4, "3": 0.2, "4":0.2,"5": 0.2}},
-            "incidence": 0.75,
-            },
-        ),
-    ),
     registered=patients.registered_as_of(
         "index_date",
         return_expectations={"incidence":0.95}
@@ -99,3 +66,20 @@ study = StudyDefinition(
         return_expectations={"incidence": 0.50},
     ),
 )
+
+measures = [
+    Measure(
+        id="ADT_rate",
+        numerator="ADT",
+        denominator="population",
+        group_by="population",
+        small_number_suppression=True,
+    ),
+    Measure(
+        id="ADTbyRegion_rate",
+        numerator="ADT",
+        denominator="population",
+        group_by="region",
+        small_number_suppression=True,
+    ),
+]
